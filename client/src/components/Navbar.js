ce7b28b64f1d6c392c7e1ded6072b07b7890529f
Ivan <ivan_mickovski@hotmail.com>
2020-09-14 14:58:37 +0200

    logo implemented in the navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,14 +1,16 @@
-import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 export default class Navbar extends Component {
   render() {
     return (
       <div className='navbar'>
 
-        <div>[SSM]<hr></hr>
-          Simple Solutions Media
-        </div>
+        <Link to="/">
+          <div>
+            <img src={"https://scontent-ber1-1.xx.fbcdn.net/v/t1.15752-9/p1080x2048/119237924_668337553888919_489692955869908853_n.png?_nc_cat=102&_nc_sid=b96e70&_nc_eui2=AeFLqdMvIJAX6tKDTeXVfaHmFX7tZrE_PhMVfu1msT8-E4H67WD2tsBjlvSv4rE2JvE&_nc_ohc=vCwAM3L5ROUAX8fX6AG&_nc_ht=scontent-ber1-1.xx&oh=503b5293af4d58896333f85aeec26f43&oe=5F834386"} />
+          </div>
+        </Link>
 
         <Link to="/"><div>Services</div></Link>
         <Link to="/"><div>Solutions</div></Link>

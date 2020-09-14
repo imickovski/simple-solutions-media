4e956a0bc1178789a0c10c543a12fe3ce354c2f9
Ivan <ivan_mickovski@hotmail.com>
2020-09-14 14:07:44 +0200

    Another color for h1

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -5,15 +5,16 @@ export default class Navbar extends Component {
   render() {
     return (
       <div className='navbar'>
+
         <div>[SSM]<hr></hr>
           Simple Solutions Media
         </div>
 
-        <div><Link to="/">Services</Link></div>
-        <div><Link to="/">Solutions</Link></div>
-        <div><Link to="/about">About [SSM]</Link></div>
-        <div><Link to="/contact">Contact</Link></div>
-        <div><Link to="/strategy">Strategy</Link></div>
+        <Link to="/"><div>Services</div></Link>
+        <Link to="/"><div>Solutions</div></Link>
+        <Link to="/about"><div>About [SSM]</div></Link>
+        <Link to="/contact"><div>Contact</div></Link>
+        <Link to="/strategy"><div>Strategy</div></Link>
       </div>
     )
   }

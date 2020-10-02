b426d294d69f833170b9c7925b955dea607a8898
Ivan <ivan_mickovski@hotmail.com>
2020-10-02 15:25:42 +0200

    About route positioning and a test color

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -8,9 +8,11 @@ export default class About extends Component {
         <h1>START YOUR MARKETING JOURNEY</h1>
         <h3>We are Marketing experts to support you on your way to digitalization. Together we generate added value from your data with the use of Artificial Intelligence.</h3>
         <Link to="/strategy"><button>Our Services</button></Link>
+        <br></br>
         <div>
-          PLACE FOR MORE CONTENT ON THE MAIN PAGE
+          PLACE FOR MORE TEXT AND CONTENT FOR THE MAIN PAGE
         </div>
+
       </div>
 
 
